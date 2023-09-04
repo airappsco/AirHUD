acef595fa51b12dbc9a5df48fbbe90560ea37906
Grant Kerr <grant.kerr@airapps.com>
2023-09-04 15:35:17 +0100

    IOSADA-629: Fixing lint error in Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AirHUD",
-            targets: ["AirHUD"]),
+            targets: ["AirHUD"]
+        )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
