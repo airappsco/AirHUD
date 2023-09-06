16c25493a5933d8825b9faab9ceb580cb652d823
Grant Kerr <grant.kerr@airapps.com>
2023-09-06 17:18:24 +0100

    IOSADA-622: removed swiftlint

--- Package.swift
+++ Package.swift
@@ -18,8 +18,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint.git",
-            from: "0.52.4")
+//        .package(
+//            url: "https://github.com/realm/SwiftLint.git",
+//            from: "0.52.4"
+//        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
