995e344cfbe5832a91409ceec0e10926db74591b
Grant Kerr <grant.kerr@airapps.com>
2023-09-06 20:09:30 +0100

    IOSADA-622: Reintroduce SwiftLint plugin

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(
-//            url: "https://github.com/realm/SwiftLint.git",
-//            from: "0.52.4"
-//        )
+        .package(
+            url: "https://github.com/realm/SwiftLint.git",
+            from: "0.52.4"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,14 +29,14 @@ let package = Package(
         .target(
             name: "AirHUD",
             dependencies: [],
-            path: "Sources/AirHUD"//,
-            //plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+            path: "Sources/AirHUD",
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
         ),
         .testTarget(
             name: "AirHUDTests",
             dependencies: ["AirHUD"],
-            path: "Tests/AirHUDTests"//,
-            //plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+            path: "Tests/AirHUDTests",
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
         )
     ]
 )
