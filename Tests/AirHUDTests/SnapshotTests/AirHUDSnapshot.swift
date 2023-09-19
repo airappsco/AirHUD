a69eeb036542b5fbaca29ff4e20631dbb15ce9e8
Ufuk Benlice <ufuk.benlice@airapps.com>
2023-09-19 11:53:27 +0300

    Add snapshot tests

---
+++ Tests/AirHUDTests/SnapshotTests/AirHUDSnapshot.swift
@@ -0,0 +1,120 @@
+//
+//  Snapshot.swift
+//  AirHUD
+//
+//  Created by Ufuk Benlice on 19/09/23.
+//  Copyright ©2023 AirApps. All rights reserved.
+//
+
+import SnapshotTesting
+import SwiftUI
+import XCTest
+
+protocol AirHUDSnapshotting: XCTestCase {
+    var fileName: StaticString { get }
+    
+    var className: String { get }
+}
+
+extension AirHUDSnapshotting {
+    
+    var allDevices: [String: ViewImageConfig] {
+        [
+            "iPhoneSe": .iPhoneSe,
+            "iPhoneXsMax": .iPhoneXsMax,
+            "iPhone13Pro": .iPhone13Pro,
+            "iPadMini": .iPadMini,
+            "iPadMini(.landscape)": .iPadMini(.landscape),
+            "iPadPro10_5": .iPadPro10_5,
+            "iPadPro12_9": .iPadPro12_9,
+            "iPadPro12_9(.landscape(splitView: .oneThird))": .iPadPro12_9(.landscape(splitView: .oneThird)),
+            "iPadPro12_9(.portrait(splitView: .twoThirds))": .iPadPro12_9(.portrait(splitView: .twoThirds)),
+            "iPadPro12_9(.portrait(splitView: .oneThird))": .iPadPro12_9(.portrait(splitView: .oneThird)),
+            "iPadPro12_9(.landscape)": .iPadPro12_9(.landscape)
+        ]
+    }
+    
+    var baseDevices: [String: ViewImageConfig] {
+        [
+            "iPhoneX": .iPhoneX,
+            "iPadPro10_5": .iPadPro10_5,
+            "iPadMini(.landscape)": .iPadMini(.landscape)
+        ]
+    }
+    
+    var dynamicSizes: [ContentSizeCategory] {
+        [
+            .extraSmall,
+            .accessibilityMedium,
+            .accessibilityExtraExtraExtraLarge
+        ]
+    }
+    
+    var darkMode: (String, UITraitCollection) {
+        ("darkMode", UITraitCollection(userInterfaceStyle: .dark))
+    }
+    
+    var lightMode: (String, UITraitCollection) {
+        ("lightMode", UITraitCollection(userInterfaceStyle: .light))
+    }
+    
+    var rtl: (String, UITraitCollection) {
+        ("rtl", UITraitCollection(layoutDirection: .rightToLeft))
+    }
+    
+    func assertViewInAllCases<SnapshotView: View>(_ view: SnapshotView, named: String, recordMode: Bool = false) {
+        let traitTuples: [(String, UITraitCollection)] = [darkMode, lightMode, rtl]
+        allDevices.forEach { device in
+            traitTuples.forEach { trait in
+                assertSnapshot(
+                    matching: view.toVC(),
+                    as: .image(on: device.value, traits: trait.1),
+                    named: "\(named)-\(device.key)-Trait:\(trait.0)",
+                    record: recordMode,
+                    file: fileName,
+                    testName: className)
+            }
+            dynamicSizes.forEach { contentSize in
+                assertSnapshot(
+                    matching: view.environment(\.sizeCategory, contentSize).toVC(),
+                    as: .image(on: device.value),
+                    named: "\(named)-\(device.key)-DynamicSize:\(contentSize)",
+                    record: recordMode,
+                    file: fileName,
+                    testName: className)
+            }
+        }
+    }
+    
+    func assertView<SnapshotView: View>(_ view: SnapshotView,
+                                        named: String,
+                                        trait: (String, UITraitCollection)? = nil,
+                                        recordMode: Bool = false) {
+        let selectedTrait = trait ?? lightMode
+        baseDevices.forEach { device in
+            assertSnapshot(
+                matching: view.toVC(),
+                as: .image(on: device.1, traits: selectedTrait.1),
+                named: "\(named)-\(device.key)-Trait:\(selectedTrait.0)",
+                record: recordMode,
+                file: fileName,
+                testName: className
+            )
+        }
+    }
+}
+
+extension SwiftUI.View {
+    func toVC() -> UIViewController {
+        let viewController = UIHostingController(rootView: self)
+        viewController.view.frame = UIScreen.main.bounds
+        return viewController
+    }
+    
+    func toNavigation() -> UINavigationController {
+        let viewController = UIHostingController(rootView: self)
+        viewController.view.frame = UIScreen.main.bounds
+        let navigation = UINavigationController(rootViewController: viewController)
+        return navigation
+    }
+}
---
+++ Tests/AirHUDTests/SnapshotTests/AirHUDViewTests.swift
@@ -0,0 +1,89 @@
+//
+//  AirHUDViewTests.swift
+//  AirHUD
+//
+//  Created by Ufuk Benlice on 19/09/23.
+//  Copyright ©2023 AirApps. All rights reserved.
+//
+
+@testable import AirHUD
+import SnapshotTesting
+import SwiftUI
+import XCTest
+
+// swiftlint:disable implicitly_unwrapped_optional
+final class AirHUDViewTests: XCTestCase, AirHUDSnapshotting {
+    
+    var fileName: StaticString = #file
+    var className = String(describing: AirHUDViewTests.self)
+    
+    var recordMode: Bool = true
+    
+    func test_ShowIconAndTitle() {
+        let sut = Spacer()
+            .airHud(isPresented: .constant(true),
+                    iconImage: Image(systemName: "doc.on.doc"),
+                    iconColor: Color(uiColor: .systemBlue),
+                    title: "Text Copied")
+        
+        assertView(sut, named: "test_ShowIconAndTitle", trait: lightMode, recordMode: recordMode)
+        assertView(sut, named: "test_ShowIconAndTitle", trait: darkMode, recordMode: recordMode)
+    }
+    
+    func test_ShowTitleAndButton() {
+        let sut = Spacer()
+            .airHud(isPresented: .constant(true),
+                    iconImage: Image(systemName: "trash"),
+                    iconColor: Color(uiColor: .systemRed),
+                    title: "Conversation Deleted",
+                    buttonTitle: "Undo",
+                    buttonAction: nil)
+        
+        assertView(sut, named: "test_ShowTitleAndButton", trait: lightMode, recordMode: recordMode)
+        assertView(sut, named: "test_ShowTitleAndButton", trait: darkMode, recordMode: recordMode)
+    }
+    
+    func test_ShowTitleAndSubtitle() {
+        let sut = Spacer()
+            .airHud(isPresented: .constant(true),
+                    iconImage: Image(systemName: "folder"),
+                    iconColor: Color(uiColor: .systemBlue),
+                    title: "Moved",
+                    subtitle: "File moved to \"Personal\"")
+        
+        assertView(sut, named: "test_ShowTitleAndSubtitle", trait: lightMode, recordMode: recordMode)
+        assertView(sut, named: "test_ShowTitleAndSubtitle", trait: darkMode, recordMode: recordMode)
+    }
+    
+    func test_Customized() {
+        var customizedConfiguration: AirHUDConfiguration {
+            let iconConfiguration = IconConfiguration(image: .init(systemName: "star"),
+                                                      color: .white,
+                                                      position: .trailing,
+                                                      size: CGSize(width: 60, height: 60))
+            let titleConfiguration = TitleConfiguration(text: "Added to Favorites",
+                                                        color: .purple,
+                                                        font: .headline.italic())
+            let subtitleConfiguration = SubtitleConfiguration(text: "You can change your favorites at any time from favorites section",
+                                                              color: .blue,
+                                                              font: .callout)
+            let dismissConfiguration = DismissConfiguration(autoDismiss: false)
+            let generalConfiguration = GeneralConfiguration(backgroundColor: .orange,
+                                                            horizontalAlingment: .center,
+                                                            verticalAlignment: .bottom)
+            let mode: AirHUDMode = .iconTitleAndSubtitle(icon: iconConfiguration,
+                                                         title: titleConfiguration,
+                                                         subtitle: subtitleConfiguration,
+                                                         dismiss: dismissConfiguration,
+                                                         general: generalConfiguration)
+            return AirHUDConfiguration(mode: mode)
+        }
+        
+        let sut = Spacer()
+            .airHud(isPresented: .constant(true),
+                    configuration: customizedConfiguration)
+                
+        assertView(sut, named: "test_Customized", trait: lightMode, recordMode: recordMode)
+        assertView(sut, named: "test_Customized", trait: darkMode, recordMode: recordMode)
+    }
+}
